61294cdce1f79044c0a2bd21cb0dec84ba16aedc
rupalisarcar <rupalisarcar@gmail.com>
2025-09-30 20:56:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const openBtn = document.getElementById('openModal');
+const body = document.getElementById('mymodal');
+const closeBtn = document.getElementById('close-modal');
+
+openBtn.addEventListener('click',()=>{
+	body.style.display="block"
+})
+closeBtn.addEventListener('click',()=>{
+	body.style.display="none"
+})
+
+window.addEventListener('click',(e)=>{
+	if(e.target===body){
+		body.style.display="none"
+	}
+})
